daf11c1f8570085cc4a81ab35a7dd190dab1cd52
elbob-mx <rob7th@gmail.com>
2022-02-03 10:44:21 -0600

    V 1.0

--- hangman_js.js
+++ hangman_js.js
@@ -8,3 +8,11 @@ var words = [
   "ALICE IN CHAINS",
   "GOJIRA"
 ]
+
+/* buscar y declarar random en el arreglo words,
+buscar letra dentro de la palabra,
+si esta, se imprime
+definir lugar de la letra,
+si no, alert "intenta de nuevo" y dibujar parte del mono
+depues de 'equis' intentos, game over
+si se complete la palabra, alert 'felicidades' */
