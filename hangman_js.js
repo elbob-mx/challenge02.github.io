e0b5457a22cbcd2e6584e4ac22b62e9dffb5cb67
elbob-mx <rob7th@gmail.com>
2022-02-08 21:15:04 -0600

    V 1.0

--- hangman_js.js
+++ hangman_js.js
@@ -2,11 +2,13 @@
 
 var words = [
   "HEMINGWAY",
+  "BEAUVOIR",
   "HOSSEINI",
   "BRONTE",
   "DOSTOYEVSKI",
   "TWAIN",
   "BORGES",
+  "ATWOOD",
   "CORTAZAR",
   "MELVILLE",
   "LOVECRAFT",
@@ -18,7 +20,9 @@ var words = [
   "WOOLF",
   "ASIMOV",
   "HUXLEY",
-  "RAND"
+  "RAND",
+  "MOORE",
+  "LAGARDE",
 ];
 console.log(words.length);
 console.log(words[0]);
@@ -28,10 +32,12 @@ console.log(words[0]);
 const getRandomWord = words[Math.floor(Math.random() * words.length)];
 let randomWord = getRandomWord.split("");
 
+
+
 /* buscar y declarar random en el arreglo 'words',
 buscar letra dentro de la palabra,
-si esta, se imprime
+si está, se imprime
 definir lugar de la letra,
 si no, alert "intenta de nuevo" y dibujar parte del mono
 depues de 'equis' intentos, game over
-si se complete la palabra, alert 'felicidades' */
+si se complete la palabra, alert 'FRASE ESCRITOR' */
