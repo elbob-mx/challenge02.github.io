8f4da3a982f1ee45af2b836f17e9b43b912f3554
elbob-mx <rob7th@gmail.com>
2022-02-10 13:54:00 -0600

    V 1.0

--- hangman_js.js
+++ hangman_js.js
@@ -49,51 +49,51 @@ var author = document.getElementById('randomWord');
 
 // FUNCIONES --------------------------->
 
-function addTo() { 
-  authorsArr.push(document.getElementById("addWords").value); 
+function addTo() {
+  authorsArr.push(document.getElementById("addWords").value);
   console.log(authorsArr); //confirmación
-} 
+}
 
 // crear espacios para letras ------>
 
 function createWordLetters() {
   var newDiv = document.createElement("lettersDiv");
   var newContent = document.createTextNode("Comencemos");
-  newDiv.appendChild(newContent); 
+  newDiv.appendChild(newContent);
 }
 
 function spellRandomWord() {
   var authorList = document.createElement('li');
-    authorList.setAtribute("class", "thisAuthor");
-    authorList.appendChild(authorList);
+  authorList.setAtribute("class", "thisAuthor");
+  authorList.appendChild(authorList);
 }
 
 // botón iniciar juego ------->
 
 // frases ganadoras -------->
 
-if(/* ACIERTA */) {
+if (/* ACIERTA */) {
   else if (
     getRandomWord == [0] {
-      document.print("El secreto de la sabiduría, del poder y del conocimiento es la humildad");
+    document.print("El secreto de la sabiduría, del poder y del conocimiento es la humildad");
   };
   else if (
     getRandomWord == [1] {
-      document.print("Una mujer libre es justo lo contrario de una mujer fácil");
-    }
-    else if (
-      getRandomWord == [2] {
-        document.print("Ser cobarde no tiene nada de malo mientras vaya acompañado de la prudencia. Pero cuando el cobarde deja de recordar quién es..., que Dios lo ayude.");
-      }
-      else if (
-        getRandomWord == [3] {
-          document.print("Las personas orgullosas engendran tristezas por sí mismas.");
-        }
-        else if (
-          getRandomWord == [4] {
-            document.print("Hay que querer hasta el extremo de alcanzar el fin; todo lo demás son insignificancias.");
-          }
-    
+    document.print("Una mujer libre es justo lo contrario de una mujer fácil");
+  }
+  else if (
+    getRandomWord == [2] {
+    document.print("Ser cobarde no tiene nada de malo mientras vaya acompañado de la prudencia. Pero cuando el cobarde deja de recordar quién es..., que Dios lo ayude.");
+  }
+  else if (
+    getRandomWord == [3] {
+    document.print("Las personas orgullosas engendran tristezas por sí mismas.");
+  }
+  else if (
+    getRandomWord == [4] {
+    document.print("Hay que querer hasta el extremo de alcanzar el fin; todo lo demás son insignificancias.");
+  }
+
 }
 
 
