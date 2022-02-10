d799708b92076eea0a3d60a41d5dd757ad3657d0
elbob-mx <rob7th@gmail.com>
2022-02-09 23:16:39 -0600

    V 1.0

--- hangman_js.js
+++ hangman_js.js
@@ -38,12 +38,15 @@ console.log(authorsArr[0]);
 
 // variables -------->
 
-const
-  getRandomWord = authorsArr[Math.floor(Math.random() * authorsArr.length)];
+const getRandomWord = authorsArr[Math.floor(Math.random() * authorsArr.length)];
 let randomWord = getRandomWord.split("");
 
 console.log(getRandomWord);
 console.log(randomWord);
+
+
+var author = document.getElementById('randomWord');
+
 
 // funciones ----------->
 
@@ -52,6 +55,20 @@ function addTo() {
   console.log(authorsArr); //confirmación
 } 
 
+// crear espacios para letras ------>
+
+function createWordLetters() {
+  var newDiv = document.createElement("lettersDiv");
+  var newContent = document.createTextNode("Shall we start?");
+	//agregue el nodo de texto al div recién creado
+  newDiv.appendChild(newContent); 
+}
+
+function spellRandomWord() {
+  var authorList = document.createElement('li');
+    authorList.setAtribute("class", "thisAuthor");
+    fruta.appendChild(authorList);
+}
 
 
 
