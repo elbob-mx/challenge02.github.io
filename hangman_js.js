1cc18de9efdc83e14029f07a7ec8b31bd852fd70
elbob-mx <rob7th@gmail.com>
2022-02-10 18:10:54 -0600

    Merge branch 'master' of https://github.com/elbob-mx/challenge-02

--- hangman_js.js
+++ hangman_js.js
@@ -24,13 +24,10 @@ var authorsArr = [
   "MOORE",
   "LAGARDE",
   "BUTLER",
-  "ATWOOD",
   "BARDUGO",
   "HURLEY",
-  "MONTERO",
-  "CLARKE",
-  "HOBB",
   "JEMISIN",
+  "BROWN"
 ];
 console.log(authorsArr.length);
 console.log(authorsArr[0]);
@@ -74,9 +71,9 @@ function spellRandomWord() {
 
 if (/* ACIERTA */) { alert
   else if (
-    getRandomWord == [0] {
+    getRandomWord == [0] else {
     document.print("El secreto de la sabiduría, del poder y del conocimiento es la humildad");
-  };
+  }
   else if (
     getRandomWord == [1] {
     document.print("Una mujer libre es justo lo contrario de una mujer fácil");
@@ -93,7 +90,94 @@ if (/* ACIERTA */) { alert
     getRandomWord == [4] {
     document.print("Hay que querer hasta el extremo de alcanzar el fin; todo lo demás son insignificancias.");
   }
-
+  else if (
+    getRandomWord == [5] {
+    document.print("Un hombre con una idea nueva es un loco hasta que la idea triunfa.");
+  }
+  else if (
+    getRandomWord == [6] {
+    document.print("La duda es uno de los nombres de la inteligencia.");
+  }
+  else if (
+    getRandomWord == [7] {
+    document.print("No sabes de lo que eres capaz hasta que tienes que hacerlo.");
+  }
+  else if (
+    getRandomWord == [8] {
+    document.print("La explicación es un error bien vestido.");
+  }
+  else if (
+    getRandomWord == [9] {
+    document.print("La verdad contada de modo inflexible tendrá siempre sus lados escabrosos.");
+  }
+  else if (
+    getRandomWord == authorsArr[10] {
+    document.print("El proceso de profundizar en el abismo negro es para mí la forma más aguda de fascinación.");
+  }
+  else if (
+    getRandomWord == authorsArr[11] {
+    document.print("Si no hubiera quienes triunfan contra toda probabilidad, creo que todo el mundo renunciaría.");
+  }
+  else if (
+    getRandomWord == authorsArr[12] {
+    document.print("La felicidad consiste en ver que la vida de uno en su totalidad tiene sentido y vale la pena.");
+  }
+  else if (
+    getRandomWord == authorsArr[13] {
+    document.print("Cuando alguien que de verdad necesita algo, lo encuentra, no es la casualidad quien lo procura, sino él mismo. Su propio deseo y su propia necesidad le conducen a ello.");
+  }
+  else if (
+    getRandomWord == authorsArr[14] {
+    document.print("Inspiré profundamente y escuché el antiguo estribillo de mi corazón. Yo soy yo soy yo soy.");
+  }
+  else if (
+    getRandomWord == authorsArr[15] {
+    document.print("Nos pasamos años sin vivir en absoluto, y de pronto toda nuestra vida se concentra en un solo instante.");
+  }
+  else if (
+    getRandomWord == authorsArr[16] {
+    document.print("Durante la mayor parte de la historia, 'Anónimo' era una mujer.");
+  }
+  else if (
+    getRandomWord == authorsArr[17] {
+    document.print("En presencia de la Oscuridad total, la mente cree que es absolutamente necesario crear luz.");
+  }
+  else if (
+    getRandomWord == authorsArr[18] {
+    document.print("La realidad no es lo que nos sucede, sino lo que hacemos con lo que nos sucede.");
+  }
+  else if (
+    getRandomWord == authorsArr[19] {
+    document.print("¿Te sientes miserablemente indefenso y quieres rebelarte? Rebélate contra las ideas de tus maestros.");
+  }
+  else if (
+    getRandomWord == authorsArr[20] {
+    document.print("El pasado no puede hacerte daño, no a menos que se lo permitas.");
+  }
+  else if (
+    getRandomWord == authorsArr[21] {
+    document.print("Nos han enseñado a tener miedo a la libertad; miedo a tomar decisiones, miedo a la soledad. El miedo a la soledad es un gran impedimento en la construcción de la autonomía, porque desde muy pequeñas y toda la vida se nos ha formado en el sentimiento de orfandad.");
+  }
+  else if (
+    getRandomWord == authorsArr[22] {
+    document.print("Cualquiera que sea la libertad por la que luchamos, debe ser una libertad basada en la igualdad.");
+  }
+  else if (
+    getRandomWord == authorsArr[23] {
+    document.print("Los hechos estan para los que no tienen imaginacion.");
+  }
+  else if (
+    getRandomWord == authorsArr[24] {
+    document.print("Tu voz es poderosa. Tu voz tiene significado, si no lo tuviera, no se esforzarían tanto en silenciarla.");
+  }
+  else if (
+    getRandomWord == authorsArr[25] {
+    document.print("La magia es meramente comunicación y conductas.");
+  }
+  else if (
+    getRandomWord == authorsArr[25] {
+    document.print("Dales todo lo que tienes, pero no les enseñes todo lo que sabes.");
+  }
 }
 
 
