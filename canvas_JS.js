24cbadadb2a869f1187a31e1c8ac3a2cc35260f8
elbob-mx <rob7th@gmail.com>
2022-02-13 18:51:35 -0600

    V 1.0

---
+++ canvas_JS.js
@@ -0,0 +1,66 @@
+var pantalla = document.querySelector("#ahorcado");
+var stroke = pantalla.getContext("2d");
+
+
+function horca(){
+    stroke.fillStyle = "#FFDEAD"
+    stroke.beginPath();
+    stroke.moveTo(100,600);
+    stroke.lineTo(50,620);
+    stroke.lineTo(150,620);
+    stroke.fill();
+    stroke.fillRect(95,205,10,400);
+    stroke.fillRect(95,205,200,10);
+    stroke.fillRect(295,205,10,50);
+}
+function head(){
+    stroke.fillStyle = "#FFDEAD"
+    stroke.beginPath();
+    stroke.arc(300,295,40,0,2*3.14);
+    stroke.fill();
+}
+
+function body(){
+    stroke.fillStyle = "#FFDEAD"
+    stroke.fillRect(295,335,10,130);
+}
+
+function leftArm(){
+    stroke.fillStyle = "#FFDEAD"
+    stroke.beginPath();
+    stroke.moveTo(295,355);
+    stroke.lineTo(220,325);
+    stroke.lineTo(220,335);
+    stroke.lineTo(295,365);
+    stroke.fill();
+}
+
+function rightArm(){
+    stroke.fillStyle = "#FFDEAD"
+    stroke.beginPath();
+    stroke.moveTo(305,355);
+    stroke.lineTo(380,325);
+    stroke.lineTo(380,335);
+    stroke.lineTo(305,365);
+    stroke.fill();
+}
+
+function leftLeg(){
+    stroke.fillStyle = "#FFDEAD"
+    stroke.beginPath();
+    stroke.moveTo(295,455);
+    stroke.lineTo(210,505);
+    stroke.lineTo(210,515);
+    stroke.lineTo(295,465);
+    stroke.fill();
+}
+
+function rightLeg(){
+    stroke.fillStyle = "#FFDEAD"
+    stroke.beginPath();
+    stroke.moveTo(305,455);
+    stroke.lineTo(390,505);
+    stroke.lineTo(390,515);
+    stroke.lineTo(305,465);
+    stroke.fill();
+}
--- hangman_js.js
+++ hangman_js.js
@@ -38,183 +38,195 @@ var addWordInput = document.querySelector("#addWordInput");
 var addWordBtn = document.querySelector("#addWordBtn");
 var resetButton = document.querySelector("#reset-button");
 
-
 var initGame = false;
 var randomAuthor;
 var indexes = [];
-var arrayPalabra;
-arrayLetraIngresada = [];
-arrayLetrasCorrectas = [];
-arrayLetrasIncorrectas = [];
-let letrasUnicas = [];
-//--------funcion para generar un array sin letras repetidas
-//--------que sera usado para verificar el ganador
-function cribarLetrasRepetidas(){
-    for (i=0;i<randomAuthor.length;i++){
-        if(!letrasUnicas.includes(randomAuthor[i])){
-            letrasUnicas.push(randomAuthor[i])
+var tempArray;
+userLetterArr = [];
+correctLetterArr = [];
+wrongLetterArr = [];
+let soloLetters = [];
+
+
+// FUNCIONES ========================>
+
+// función no repetir letras --------------->
+function nonRepeatLetters() {
+    for(i=0;i<randomAuthor.length;i++){
+        if(!soloLetters.includes(randomAuthor[i])){
+            soloLetters.push(randomAuthor[i])
         }
     }
 }
-//--------sortea una palabra aleatoria del array original
-//--------luego elimina esa palabra del array para que no repita
-function sortearPalabra(){
-    var numeroAleatorio = Math.floor(Math.random()*palabrasSecretas.length);
-    randomAuthor = palabrasSecretas[numeroAleatorio];
-    palabrasSecretas.splice(numeroAleatorio,1);
+
+// autor random y eliminado ----------------------->
+function selectRandomAuthor() {
+    var randomInt = Math.floor(Math.random()*authorArr.length);
+    randomAuthor = authorArr[randomInt];
+    authorArr.splice(randomInt,1);
     return randomAuthor;
 }
-//--------crea un array para ser usado como referencia para el juego
-function crearArrayPalabra(palabra){
-    separada = palabra.split("");
-    arrayPalabra = separada;
+
+// crear array temporal por juego -------------------->
+function tempArr(palabra) {
+    splitWord = palabra.split("");
+    tempArray = splitWord;
 }
-//--------asigna la funcionalidad a los botones de inicio y reinicio
-startGameBtn.addEventListener("click",function(event){
+
+// funciones de botones --------------------------->
+startGameBtn.addEventListener("click",function(event) {
     event.preventDefault();
-    pincel.clearRect(0, 0, pantalla.width, pantalla.height);
-    iniciarJuego();
+    stroke.clearRect(0, 0, screen.width, screen.height);
+    playGame();
 })
-btnReiniciar.addEventListener("click",function(event){
+
+resetButton.addEventListener("click",function(event) {
     event.preventDefault();
-    pincel.clearRect(0, 0, pantalla.width, pantalla.height);
-    iniciarJuego();
+    stroke.clearRect(0, 0, screen.width, screen.height);
+    playGame();
 })
-//--------permite al usuario agregar sus propias palabras
-btnAgregarPalabra.addEventListener("click",function(event){
+
+// agregar palabras usuario ------------------------------>
+addWordBtn.addEventListener("click",function(event) {
     event.preventDefault();
-    palabrasSecretas.push(addWordInput.value.toUpperCase());
+    authorArr.push(addWordInput.value.toUpperCase());
     addWordInput.value = "";
     addWordInput.focus();
 })
-//--------nuclea las acciones necesarias para iniciar un nuevo juego
-function iniciarJuego(){
-    pincel.clearRect(0, 0, pantalla.width, pantalla.height);
-    patibulo();
-    sortearPalabra();
-    crearArrayPalabra(randomAuthor);
-    dibujarGuiones();
-    cribarLetrasRepetidas();
+
+// arrancar juego -------------------------------->
+function playGame() {
+    stroke.clearRect(0, 0, screen.width, screen.height);
+    horca();
+    selectRandomAuthor();
+    tempArr(randomAuthor);
+    printUnderscore();
+    nonRepeatLetters();
     initGame = true;
-    arrayLetraIngresada = [];
-    arrayLetrasCorrectas = [];
-    arrayLetrasIncorrectas = [];
+    userLetterArr = [];
+    correctLetterArr = [];
+    wrongLetterArr = [];
 }
+
+
 //--------genera un array con los indexes de las letras ingresadas 
 //--------por los usuarios, esto permite que si hay letras repetidas
 //--------dentro de la palabra original pueda dibujar todas
 //--------las instancias de esa letra
-function buscarindexes(){
+function searchIndexes() {
     if (initGame){
-    var indiceBuscado = arrayPalabra.indexOf(arrayLetraIngresada[0]);
-        while (indiceBuscado != -1) { //el -1 es el return de indexOf si no encuentra el elemento
-            indexes.push(indiceBuscado);
-            indiceBuscado = arrayPalabra.indexOf(arrayLetraIngresada[0], indiceBuscado + 1);
+    var indexSeek = tempArray.indexOf(userLetterArr[0]);
+        while (indexSeek != -1) { //el -1 es el return de indexOf si no encuentra el elemento
+            indexes.push(indexSeek);
+            indexSeek = tempArray.indexOf(userLetterArr[0], indexSeek + 1);
+    }
   }
 }
-}
-//--------dibuja la cantidad de guiones necesarios para la palabra en juego
-function dibujarGuiones(){
-    var inicioX = 350;
-    var inicioY = 610;
-    var contador = 0;
-    var nLetras = randomAuthor.length;
-    while (contador<nLetras){
-        pincel.fillStyle = "red";
-        pincel.fillRect(inicioX+(40*contador),inicioY,30,4);
-        contador++;
+
+// print de guines bajos -------------------------->
+function printUnderscore() {
+    var startX = 350;
+    var startY = 610;
+    var count = 0;
+    var fooLetters = randomAuthor.length;
+    while (count<fooLetters){
+        stroke.fillStyle = "black";
+        stroke.fillRect(startX+(40*count),startY,30,4);
+        count++;
     }
 }
-//--------coloca cada letra en el lugar que deberia aparecer
-function dibujarletras(arrOrden){
-    var inicioX = 358;
-    var inicioY = 600;
-        for(i=0;i<arrOrden.length;i++){
-            pincel.fillStyle = "blue";
-            pincel.font = "20px Georgia";
-            pincel.fillText(arrayLetraIngresada[0],inicioX+(40*arrOrden[i]),inicioY);
+// letras acertadas del usuario ------------------>
+function printLetters(orderArr) {
+    var startX = 358;
+    var startY = 600;
+        for(i=0;i<orderArr.length;i++){
+            stroke.fillStyle = "whitesmoke";
+            stroke.font = "20px Courier New";
+            stroke.fillText(userLetterArr[0],startX+(40*orderArr[i]),startY);
         }
         indexes = [];
 }
-//--------evento para capturar las teclas del usuario, en el cual
-//--------comprueba si son letras y no caracteres especiales o numeros
-//--------almacena las letras en uno de dos arrays, de acuerdo a si la
-//--------letra esta o no dentro de la palabra sorteada.
-//--------tambien dibuja la pieza del ahorcado en caso de que sea necesario
-//--------y comprueba si el juego ha terminado
-document.addEventListener("keyup", function(event){
-    arrayLetraIngresada = [];
+
+/* evento para capturar las teclas del usuario, en el cual comprueba si son letras y no caracteres especiales o numeros almacena las letras en uno de dos arrays, de acuerdo a si la letra esta o no dentro de la palabra sorteada.
+tambien dibuja la pieza del ahorcado en caso de que sea necesario y comprueba si el juego ha terminado */
+
+document.addEventListener("keyup", function(event) {
+    userLetterArr = [];
     var letra = event.key.toUpperCase();
     var codigo = letra.charCodeAt();
-    if (initGame){
-    if(codigo>64 && codigo<91){
-        arrayLetraIngresada.push(letra);
-        buscarindexes();
-        dibujarletras(indexes);
-        var comparador = arrayLetrasIncorrectas.length;
-        if(arrayPalabra.includes(letra)){
-            if(!arrayLetrasCorrectas.includes(letra)){
-                arrayLetrasCorrectas.push(letra)
+    if (initGame) {
+    if (codigo>64 && codigo<91) {
+        userLetterArr.push(letra);
+        searchIndexes();
+        printLetters(indexes);
+        var comparador = wrongLetterArr.length;
+        if(tempArray.includes(letra)) {
+            if(!correctLetterArr.includes(letra)) {
+                correctLetterArr.push(letra)
             }
-        }else if(!arrayLetrasIncorrectas.includes(letra)){
-            arrayLetrasIncorrectas.push(letra)
+        } else if(!wrongLetterArr.includes(letra)){
+            wrongLetterArr.push(letra)
         }
-        if(comparador<arrayLetrasIncorrectas.length){
-            dibujarLetrasErroneas(arrayLetrasIncorrectas) 
+        if(comparador<wrongLetterArr.length) {
+            printWrongLetetrs(wrongLetterArr) 
         }
-        dibujarAhorcado();
+        printGallow();
         }
-    verificarFinGanador();
-    verificarFinPerdedor();
+    checkWinner();
+    checkFail();
     } 
 });
-//--------dibuja las letras que no estan en la palabra sorteada
-function dibujarLetrasErroneas(letrasIncorrectas){
-    var inicioX = 400;
-    var inicioY = 200;
-    pincel.fillStyle = "black";
-    pincel.font = "20px Georgia";
-    pincel.fillText("letras erroneas " + letrasIncorrectas.toString(),inicioX,inicioY);
+
+
+// print letras erroneas -------------------------->
+function printWrongLetetrs(failLetter) {
+    var startX = 400;
+    var startY = 200;
+    stroke.fillStyle = "black";
+    stroke.font = "20px Courier New";
+    stroke.fillText("letras erroneas " + failLetter.toString(),startX,startY);
 }
-//--------comprueba si el juego ha terminado con resultado positivo
-function verificarFinGanador(){
-    let palabraOriginalsinLetrasRepetidas = letrasUnicas.sort().toString();
-    let letrasErroneasIngresadas = arrayLetrasCorrectas.sort().toString();
-    if(palabraOriginalsinLetrasRepetidas===letrasErroneasIngresadas){
-        pincel.fillStyle = "lightgreen";
-        pincel.font = "50px Georgia";
-        pincel.fillText("Ganaste, felicidades!",600,400);
+
+// situación ganador ------------------->
+function checkWinner() {
+    let palabraOriginalsifooLettersRepetidas = soloLetters.sort().toString();
+    let letrasErroneasIngresadas = correctLetterArr.sort().toString();
+    if(palabraOriginalsifooLettersRepetidas===letrasErroneasIngresadas){
+        stroke.fillStyle = "gold";
+        stroke.font = "40px Courier New";
+        stroke.fillText("NERD!",600,400);
         initGame = false;
-        btnReiniciar.focus();
-        letrasUnicas = [];
+        reset-button.focus();
+        soloLetters = [];
     }
 }
-//--------comprueba si el juego ha terminado con resultado negativo
-function verificarFinPerdedor(){
-    if(arrayLetrasIncorrectas.length>5){
-        pincel.fillStyle = "red";
-        pincel.font = "50px Georgia";
-        pincel.fillText("Fin del juego!",600,400);
+// situación perdedor ---------------------->
+function checkFail(){
+    if(wrongLetterArr.length>5) {
+        stroke.fillStyle = "tomato";
+        stroke.font = "40px Courier New";
+        stroke.fillText("Hay que leer más...",600,400);
         initGame = false;
         alert("la palabra era " + randomAuthor);
-        btnReiniciar.focus();
-        letrasUnicas = [];
+        reset-button.focus();
+        soloLetters = [];
     }
 }
-//--------dibuja la pieza del ahorcado correspondiente segun la cantidad de errores
-function dibujarAhorcado(){
-    let contador = arrayLetrasIncorrectas.length;
-    if (contador===1){
-        cabeza()
-    }else if(contador===2){
-        cuerpo()
-    }else if(contador===3){
-        brazoIzquierdo()
-    }else if(contador===4){
-        brazoDerecho()
-    }else if(contador===5){
-        piernaIzquierda()
-    }else if(contador===6){
-        piernaDerecha()
+
+// partes del ahorcado dependiendo errores ------------->
+function printGallow(){
+    let count = wrongLetterArr.length;
+    if (count===1){
+        head()
+    }else if(count===2){
+        body()
+    }else if(count===3){
+        leftArm()
+    }else if(count===4){
+        rightArm()
+    }else if(count===5){
+        leftLeg()
+    }else if(count===6){
+        rightLeg()
     }
 }
